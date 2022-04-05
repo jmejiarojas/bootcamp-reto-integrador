6b27205ceeb02efc024be7af3e2794ff5a2642e7
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2022-04-05 15:06:53 -0500

    Add mongodb container

---
+++ bd-mongo/mongo-init.sh
@@ -0,0 +1,30 @@
+conn = new Mongo();
+db = conn.getDB("mongodb-integrador");
+
+
+#db.myCollectionName.createIndex({ "address.zip": 1 }, { unique: false });
+db.favorites.insert(
+[
+    { 
+        "nombre": "Pago a Edelnor", 
+        "tipo": "Servicios Basicos",
+        "codigoServicio":"SER001"
+    },
+    {
+        "nombre": "Pago a Calida", 
+        "tipo": "Servicios Basicos" ,
+        "codigoServicio": "SER002"
+    },
+    { 
+        "nombre": "Pago a Movistar", 
+        "tipo": "Servicios Telefonicos",
+        "codigoServicio":"SER003"
+    },
+    { 
+        "nombre": "Pago a Sedapal", 
+        "tipo": "Servicios Basicos",
+        "codigoServicio":"SER004" 
+    }
+]
+);
+
