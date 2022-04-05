0d6cd8c8f7317a3b26125886b9433c1e4554219b
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2022-04-05 12:38:35 -0500

    Add dokerfile mssql

---
+++ bd-mssql/setup.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE integrador;
+GO
+
+USE integrador;
+GO
+
+create table services(
+                         id int identity(1,1) primary key,
+                         serviceCode varchar(50),
+                         channelCode varchar(50),
+                         name varchar(250),
+);
+
+create table transactions(
+                             id int identity(1,1) primary key,
+                             serviceCode varchar(50),
+                             supplyNumber varchar(50),
+                             amount NUMERIC,
+                             dateTime DATETIME
+);
+
+insert into [dbo].[services](serviceCode, channelCode, name) values('SE001','NTLC', 'Transferencias');
+insert into [dbo].[services](serviceCode, channelCode, name) values('SE002','NTLC', 'Carta Fianza');
+insert into [dbo].[services](serviceCode, channelCode, name) values('SE003','TLC', 'AutoDesembolso');
