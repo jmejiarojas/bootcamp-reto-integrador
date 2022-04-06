3db86df2a01595b0cb1379dd0b3773f8e445e24d
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2022-04-05 19:51:16 -0500

    Update script relational db

--- bd-mssql/setup.sql
+++ bd-mssql/setup.sql
@@ -1,24 +1,27 @@
-CREATE DATABASE integrador;
-GO
+use master;
 
-USE integrador;
-GO
+create database integrador;
+
+use integrador;
 
 create table services(
-                         id int identity(1,1) primary key,
-                         serviceCode varchar(50),
-                         channelCode varchar(50),
-                         name varchar(250),
+    id int identity(1,1) primary key,
+    serviceCode varchar(50),
+    channelCode varchar(50),
+    name varchar(250),
 );
 
 create table transactions(
-                             id int identity(1,1) primary key,
-                             serviceCode varchar(50),
-                             supplyNumber varchar(50),
-                             amount NUMERIC,
-                             dateTime DATETIME
+  id int identity(1,1) primary key,
+  channelCode varchar(50),
+  serviceCode varchar(50),
+  supplyNumber varchar(50),
+  amount NUMERIC,
+  dateTime DATETIME
 );
 
 insert into [dbo].[services](serviceCode, channelCode, name) values('SE001','NTLC', 'Transferencias');
 insert into [dbo].[services](serviceCode, channelCode, name) values('SE002','NTLC', 'Carta Fianza');
 insert into [dbo].[services](serviceCode, channelCode, name) values('SE003','TLC', 'AutoDesembolso');
+
+
